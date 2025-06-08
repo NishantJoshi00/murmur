bd3f2915fa6d57874e2df28a731828f4a0ca7444
Nishant Joshi <nishant.joshi@juspay.in>
2025-06-08 07:38:54 +0530

    fix create a favicon

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@assets': path.resolve(__dirname, 'assets'),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
